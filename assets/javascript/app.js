ed02cb3ffd4b81c79ae933b86659d1afde011df9
mohak92 <tamhane.mohak@gmail.com>
2019-10-13 00:30:13 -0400

    fixes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -24,6 +24,23 @@ postButton.addEventListener("click", function(){
   textInput.value = "";
 });
 
+var beginListening = function(){
+  database.ref('chat').on('child_added', function (snapshot) {
+    var msg = snapshot.val();
+    var msgUsernameElement = document.createElement("b");
+    msgUsernameElement.textContent = msg.username;
+    var msgTextElement = document.createElement("p");
+    msgTextElement.textContent = msg.text;
+    var msgElement = document.createElement("div");
+    msgElement.appendChild(msgUsernameElement);
+    msgElement.appendChild(msgTextElement);
+    msgElement.className = "msg";
+    document.getElementById("results").appendChild(msgElement);
+  });
+}
+
+beginListening();
+
 function showFeedback(text) {
     const feedback = document.querySelector(".feedback");
     feedback.classList.add("showItem");
