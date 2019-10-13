0a0f5950471dbd72f04f464d1afaf4f0afae2141
mohak92 <tamhane.mohak@gmail.com>
2019-10-12 23:31:16 -0400

    chat implemented

--- public/assets/javascript/app.js
+++ public/assets/javascript/app.js
@@ -7,11 +7,8 @@ var firebaseConfig = {
   messagingSenderId: "972738822273",
   appId: "1:972738822273:web:bbb7c7d7abfa5206a10d80"
 };
-
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
-
-var database = firebase.database();
 
 var usernameInput = document.querySelector("#username");
 var textInput = document.querySelector("#text");
@@ -20,7 +17,7 @@ var postButton = document.querySelector("#post");
 postButton.addEventListener("click", function(){
   var msgUser = usernameInput.value;
   var msgText = textInput.value;
-  database.ref('chat').push(Chat = {username:msgUser, text:msgText});
+  firebase.set(msgUser + " says: " + msgText);
   textInput.value = "";
 });
 
@@ -35,21 +32,17 @@ function showFeedback(text) {
 
   $('#rock').click(function(){
     $(".img-fluid").attr('src',"assets/images/rock.jpg");
-    database.ref('rps').push({move:"rock"});
   });
 
   $('#paper').click(function(){
     $(".img-fluid").attr('src',"assets/images/paper.jpg");
-    database.ref('rps').push({move:"paper"});
   });
 
   $('#scissor').click(function(){
     $(".img-fluid").attr('src',"assets/images/scissor.jpg");
-    database.ref('rps').push({move:"scissor"});
   });
 
   $('#reset').click(function(){
     $(".img-fluid").attr('src',"assets/images/question.png");
-    
   });
 
